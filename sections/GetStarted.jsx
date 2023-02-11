ee69c9f39a25d63481503217e5418c8e0b763305
mohammadali-amd <amidi1380@gmail.com>
2023-02-11 23:09:49 +0330

    Initial commit

---
+++ sections/GetStarted.jsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { StartSteps, TitleText, TypingText } from '../components';
+import { startingFeatures } from '../constants';
+
+const GetStarted = () => (
+  <section>
+    Get Started Section
+  </section>
+);
+
+export default GetStarted;
