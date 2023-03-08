2f09c818e6714edc66410337cf77a45dfab676f4
mohammadali-amd <amidi1380@gmail.com>
2023-03-08 22:09:13 +0330

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 import styles from '../styles';
 import { navVariants } from '../utils/motion';
@@ -14,11 +15,11 @@ const Navbar = () => (
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8 bg-white shadow-lg rounded-2xl p-6 bg-clip-padding bg-opacity-40`}>
       <img src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contain" />
-      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
-        METAVERSUS
-      </h2>
+      <Link className="font-extrabold text-[24px] leading-[30px] text-white" href="/about">
+        وب استودیو
+      </Link>
       <img src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain" />
     </div>
   </motion.nav>
