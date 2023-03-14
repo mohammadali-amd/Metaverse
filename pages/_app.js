8bf2d1c89e1492090ff97e3574a0b07cf10114d6
mohammadali-amd <amidi1380@gmail.com>
2023-03-14 18:19:15 +0330

    update

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,9 @@ import '../styles/globals.css';
 const MyApp = ({ Component, pageProps }) => (
   <main className="bg-primary-black overflow-hidden">
     <Head>
-      <title>Metaversus</title>
+      <title>Web Studio</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/icon.png" />
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
     </Head>
