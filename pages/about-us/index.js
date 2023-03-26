3ca7470ac1bb1ff66597cdecc596895be52bd710
mohammadali-amd <amidi1380@gmail.com>
2023-03-26 23:50:46 +0330

    update titles

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -1,7 +1,11 @@
+import Head from 'next/head';
 import { Hero, About } from '../../sections';
 
 const AboutUs = () => (
   <div className="py-12">
+    <Head>
+      <title>Web Studio - About Us</title>
+    </Head>
     <div className="gradient-02 mt-[500px] z-0" />
     <div className="relative">
       <div className="gradient-04 mt-[400px] z-0" />
--- pages/contact-us/index.js
+++ pages/contact-us/index.js
@@ -1,7 +1,11 @@
+import Head from 'next/head';
 import Contact from '../../sections/Contact';
 
 const ContactUs = () => (
   <div className="py-12">
+    <Head>
+      <title>Web Studio - Contact Us</title>
+    </Head>
     <Contact />
   </div>
 );
